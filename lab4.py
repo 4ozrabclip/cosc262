468696ba934e10ab484c76737eb50930dd89a58d
4ozrabclip <4oz.developer@gmail.com>
2024-03-16 00:49:17 +1300

    lab4 work on strongly connected graphs

--- lab4.py
+++ lab4.py
@@ -1,10 +1,3 @@
-####QUESTION 2
-# Write a function transpose(adj_list) that takes the adjacency list of a graph and returns the adjacency list of the reverse (transpose) of the graph. 
-# The returned adjacency list must follow the same format and type described in the adjacency_list question. 
-# The order of elements in inner lists does not matter; all the tests in this question sort the inners list before printing them.
-
-# For this question, the function adjacency_list is available on the server. 
-# You do not have to provide your own copy of this function (however, you can if you wish so).
 def adjacency_list(graph_str):
 
     if len(graph_str) == 0:
@@ -31,18 +24,47 @@ def adjacency_list(graph_str):
         
             
     return result
+####QUESTION 2
+# Write a function transpose(adj_list) that takes the adjacency list of a graph and returns the adjacency list of the reverse (transpose) of the graph. 
+# The returned adjacency list must follow the same format and type described in the adjacency_list question. 
+# The order of elements in inner lists does not matter; all the tests in this question sort the inners list before printing them.
 
-def transpose(adj_list):
-     pass
+# For this question, the function adjacency_list is available on the server. 
+# You do not have to provide your own copy of this function (however, you can if you wish so).
 
-graph_string = """\
-D 3
-0 1
-1 0
-0 2
-"""
 
-graph_adj_list = adjacency_list(graph_string)
-graph_transposed_adj_list = transpose(graph_adj_list)
-for i in range(len(graph_transposed_adj_list)):
-    print(i, sorted(graph_transposed_adj_list[i]))
+# def transpose(adj_list):
+#         if len(adj_list) == 0:
+#             return []
+            
+#         result = [[] for _ in range(len(adj_list))]
+
+#         for source, neighbours in enumerate(adj_list):
+#              for neighbour, w in neighbours:
+#                   result[neighbour].append((source, w))
+
+#         return result
+
+ 	
+
+# graph_string = """\
+# U 7
+# 1 2
+# 1 5
+# 1 6
+# 2 3
+# 2 5
+# 3 4
+# 4 5
+# """
+
+# graph_adj_list = adjacency_list(graph_string)
+# print(graph_adj_list)
+# graph_transposed_adj_list = transpose(graph_adj_list)
+# for i in range(len(graph_transposed_adj_list)):
+#     print(i, sorted(graph_transposed_adj_list[i]))
+
+####QUESTION 3
+def is_strongly_connected(adj_list):
+    for source, neighbours in adj_list:
+        
